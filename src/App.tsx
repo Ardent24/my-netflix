48dccea5e92a7819b735826bc13e887bd66fe8eb
Alexander Barsukov <alexander.barsukov@sibers.com>
2021-12-30 14:13:26 +0700

    add: response MoviesAPI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState, FC } from "react";
+import { IMovie } from "./interfaces/interfaces";
 
-function App() {
-  return <div className="App"/>;
-}
+export const App = () => {
+  const [movies, setMovies] = useState<IMovie[]>([]);
+  const [isError, setIsError] = useState<Boolean>(false);
 
-export default App;
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch("http://localhost:4000/movies");
+        const json = await response.json();
+        console.log(json.data);
+        setMovies((prev) => [json.data, ...prev]);
+      } catch (err) {
+        setIsError(true);
+      }
+    })();
+  }, []);
+
+  return <div className="App" />;
+};
